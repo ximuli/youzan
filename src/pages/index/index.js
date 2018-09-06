52dacfc18a634ae7604cbce3afad1a83df25eeca
ximuli <ximuli666@163.com>
2018-09-06 11:00:19 +0800

    数据获取，渲染，触底加载以及底部导航组件

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,15 +1,50 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'css/common.css'
+import './index.css'
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import axios from 'axios'
+import url from 'js/api.js'
 
-Vue.config.productionTip = false
+import { InfiniteScroll } from 'mint-ui'
+Vue.use(InfiniteScroll)
 
-/* eslint-disable no-new */
+import Foot from 'components/Foot.vue'
+
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    data: {
+        lists: null,
+        pageNum: 1,
+        pageSize: 6,
+        loading: false,
+        allLoaded: false
+    },
+    created: function() {
+        this.getLists()
+    },
+    methods: {
+        getLists: function() {
+            if (this.allLoaded) return
+            this.loading = true
+            axios.post(url.hotLists, {
+                pageNum: this.pageNum,
+                pageSize: this.pageSize
+            }).then(res=>{
+                let curLists = res.data.lists
+                if (curLists.length < this.pageSize) {
+                    this.allLoaded = true
+                }
+                if(this.lists) {
+                    this.lists = this.lists.concat(curLists)
+                }else {
+                    //第一次请求数据
+                    this.lists = curLists
+                } 
+                this.loading = false
+                this.pageNum++
+            })
+        }
+    },
+    components: {
+        Foot
+    }
 })
