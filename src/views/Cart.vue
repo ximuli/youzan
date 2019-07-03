708a028f2af9710faa2a043c95dbb9ddb3c01ba9
ximuli <ximuli666@163.com>
2019-07-03 22:41:09 +0800

    update cart.vue

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -27,18 +27,156 @@
                 <div class="price">
                   ￥98.00
                 </div>
-                <div class="quantity">
+                <div class="num">
+                  x 3
+                </div>
+                <div class="quantity" style="display: none;">
                   <span>-</span>
                   <span>0</span>
                   <span>+</span>
                 </div>
-                <!-- <div class="num">
-                  x3
-                </div> -->
               </div>
+            </div>
+            <div class="delete-btn" style="transform: translateX(90%);">
+              <span>删除</span>
+            </div>
+          </div>
+          <div class="good">
+            <span class="check"></span>
+            <div class="good-info">
+              <img
+                class="goods-img"
+                src="https://img.yzcdn.cn/upload_files/2017/07/04/Fqp4EItsHaclmGQPBoMkR1qKmKGi.jpg?imageView2/2/w/200/h/200/q/75/format/webp"
+                alt=""
+              />
+              <div class="detail">
+                <div class="title">
+                  寻找田野|最正宗的上海南汇8424 得奖最多奥运会指定西瓜1只/箱
+                  9-11斤
+                </div>
+                <div class="size">
+                  4500g-5000g
+                </div>
+                <div class="price">
+                  ￥98.00
+                </div>
+                <div class="num">
+                  x 3
+                </div>
+                <div class="quantity" style="display: none;">
+                  <span>-</span>
+                  <span>0</span>
+                  <span>+</span>
+                </div>
+              </div>
+            </div>
+            <div class="delete-btn" style="transform: translateX(90%);">
+              <span>删除</span>
             </div>
           </div>
         </div>
+      </div>
+      <div class="shop-item">
+        <div class="shop-header">
+          <span class="check checked"></span>
+          <span class="shop-name">寻找田野</span>
+          <span class="edit-btn">编辑</span>
+        </div>
+        <div class="shop-content">
+          <div class="good">
+            <span class="check checked"></span>
+            <div class="good-info">
+              <img
+                class="goods-img"
+                src="https://img.yzcdn.cn/upload_files/2017/07/04/Fqp4EItsHaclmGQPBoMkR1qKmKGi.jpg?imageView2/2/w/200/h/200/q/75/format/webp"
+                alt=""
+              />
+              <div class="detail">
+                <div class="title">
+                  寻找田野|最正宗的上海南汇8424 得奖最多奥运会指定西瓜1只/箱
+                  9-11斤
+                </div>
+                <div class="size">
+                  4500g-5000g
+                </div>
+                <div class="price">
+                  ￥98.00
+                </div>
+                <div class="num">
+                  x 3
+                </div>
+                <div class="quantity" style="display: none;">
+                  <span>-</span>
+                  <span>0</span>
+                  <span>+</span>
+                </div>
+              </div>
+            </div>
+            <div class="delete-btn" style="transform: translateX(90%);">
+              <span>删除</span>
+            </div>
+          </div>
+          <div class="good">
+            <span class="check checked"></span>
+            <div class="good-info">
+              <img
+                class="goods-img"
+                src="https://img.yzcdn.cn/upload_files/2017/07/04/Fqp4EItsHaclmGQPBoMkR1qKmKGi.jpg?imageView2/2/w/200/h/200/q/75/format/webp"
+                alt=""
+              />
+              <div class="detail">
+                <div class="title">
+                  寻找田野|最正宗的上海南汇8424 得奖最多奥运会指定西瓜1只/箱
+                  9-11斤
+                </div>
+                <div class="size">
+                  4500g-5000g
+                </div>
+                <div class="price">
+                  ￥98.00
+                </div>
+                <div class="num">
+                  x 3
+                </div>
+                <div class="quantity" style="display: none;">
+                  <span>-</span>
+                  <span>0</span>
+                  <span>+</span>
+                </div>
+              </div>
+            </div>
+            <div class="delete-btn" style="transform: translateX(90%);">
+              <span>删除</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="invalid-wrapper">
+      <div class="bar">以下商品无法一起购买</div>
+      <div class="invalid-good">
+        <img
+          class="goods-img"
+          src="https://img.yzcdn.cn/upload_files/2017/07/04/Fqp4EItsHaclmGQPBoMkR1qKmKGi.jpg?imageView2/2/w/200/h/200/q/75/format/webp"
+          alt=""
+        />
+        <div class="detail">
+          <div class="title">
+            寻找田野|最正宗的上海南汇8424 得奖最多奥运会指定西瓜1只/箱 9-11斤
+          </div>
+          <div class="size">
+            4500g-5000g
+          </div>
+          <div class="price">
+            ￥98.00
+          </div>
+          <div class="num">
+            x 3
+          </div>
+        </div>
+      </div>
+      <div class="invalid-btn-wrapper">
+        <button class="btn invalid-btn">清空失效商品</button>
       </div>
     </div>
   </div>
@@ -61,6 +199,9 @@ export default {
     background-size: contain;
     background-repeat: no-repeat;
     background-position: center;
+  }
+  .check.checked {
+    background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAMAAADW3miqAAAAS1BMVEUAAAAzhPszhPoyhPsyhPozg/s0hPwyhPw0hvs8h/9Aj/8yg/syg/ozhPsyg/r///+wz/2RvfxTl/pup/vT5P3U5f16r/u91/18sPvWF+94AAAADnRSTlMAvdnFopBcUTsREPPW8utY9+UAAADeSURBVDjLlZTrDoMgDIWpIoqX1svm9v5PumRGD7Aq7vtH8iWnFFoT4p2lousKss4bnZo4gGpFaUpOKJvUqVihipSWWIXawOn5hB4W8Smk16PX1fAl2x3La6n89pA1lpV36rOqJ5ElqN1rziwi7+PkjdOkp8gDJ2esHiYzjtaQHjZFDS2O+yxxGChMtzsiYxQGOjNAkhFhIQPixs1CWBBHHFkIA2QsR9YLYUELHMcWwoJmeo4shAGfPPCIMJT081XWlVPqm58u/31vD0J+pPLDeXPM8wsjv3r+XWL5dfgBB3w3WcIaGhAAAAAASUVORK5CYII=);
   }
   .shop-wrapper {
     .shop-item {
@@ -87,14 +228,127 @@ export default {
         }
       }
       .shop-content {
+        background-color: #fff;
         .good {
           display: flex;
+          align-items: center;
+          background-color: #fafafa;
+          position: relative;
+          margin-bottom: 10px;
           .check {
             margin: 0 10px;
             height: 60px;
             padding: 20px 0;
+            flex-shrink: 0;
+          }
+          .good-info {
+            display: flex;
+            align-items: center;
+            > .goods-img {
+              width: 90px;
+              height: 90px;
+              margin-right: 10px;
+            }
+            .detail {
+              align-self: flex-start;
+              padding-right: 10px;
+              position: relative;
+              .title {
+                font-size: 14px;
+                line-height: 20px;
+                // 文字两行后显示 ...
+                display: -webkit-box;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                -webkit-line-clamp: 2;
+                -webkit-box-orient: vertical;
+                color: #666;
+              }
+              .size {
+                font-size: 12px;
+                margin-bottom: 10px;
+              }
+              .price {
+                font-size: 14px;
+                color: #f60;
+              }
+              .num {
+                position: absolute;
+                right: 10px;
+                bottom: 0;
+                font-size: 12px;
+              }
+            }
+          }
+          .delete-btn {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            background-color: #ff4343;
+            color: #fff;
+            font-size: 12px;
+            > span {
+              left: 20px;
+              position: absolute;
+              top: 50%;
+              margin-top: -6px;
+            }
           }
         }
+      }
+    }
+  }
+  .invalid-wrapper {
+    .bar {
+      font-size: 14px;
+      color: #999;
+      text-align: center;
+      line-height: 40px;
+    }
+    .invalid-good {
+      padding: 10px;
+      display: flex;
+      position: relative;
+      background-color: #fafafa;
+      .goods-img {
+        width: 90px;
+        height: 90px;
+        margin-right: 10px;
+      }
+      .title {
+        font-size: 14px;
+        line-height: 20px;
+        // 文字两行后显示 ...
+        display: -webkit-box;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        color: #666;
+      }
+      .size {
+        font-size: 12px;
+        margin-bottom: 10px;
+      }
+      .price {
+        font-size: 14px;
+        color: #f60;
+      }
+      .num {
+        position: absolute;
+        right: 10px;
+        bottom: 0;
+      }
+    }
+    .invalid-btn-wrapper {
+      padding: 10px 0;
+      background-color: #fff;
+      text-align: center;
+      .invalid-btn {
+        color: #f60;
+        font-size: 14px;
+        padding: 6px 44px;
+        border-color: #f60;
       }
     }
   }
